e5e33ee0c58f3d8e6cc673fd01230ff92d977f17
George Harker <george@george-graphics.co.uk>
2025-10-21 12:16:33 -0700

    Remove schema suffix on zsh parser

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -2703,7 +2703,7 @@ return {
     maintainers = { '@rockorager' },
     tier = 2,
   },
-  zsh_schema = {
+  zsh = {
     install_info = {
       location = 'tree-sitter-zsh',
       revision = '9340bfd8c8d556aea1d8feb41b32600ba9f3cc33',
